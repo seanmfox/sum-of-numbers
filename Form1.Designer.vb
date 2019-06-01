c33bf65056d6e4bec6afd75e44fd2e5470b822d6
seanmfox <geekypghrunner@gmail.com>
2019-05-31 20:44:00 -0400

    Add program functionality

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,43 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 45)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(114, 41)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Enter Numbers"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(163, 45)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(103, 41)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(293, 134)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Sum of Numbers"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿'Name: Sean Fox
+'Course: CIT-161-Z03B
+'Date: 5-31-19
+'Program Name: Sum of Numbers
+'Description: Chapter 5, Programming Challenge 1 - Create an application that sums the integers from 1 to the value entered by the user.
 
+
+Public Class Form1
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim strUserInput As String = InputBox("Enter a positive integer value.", "Input Needed", "10")
+        Dim intInput As Integer = CInt(strUserInput)
+        Dim intTotal As Integer = 0
+        For index = 1 To intInput
+            intTotal += index
+        Next
+        MessageBox.Show("The sum of numbers 1 through " + intInput.ToString + " is " + intTotal.ToString)
+    End Sub
 End Class
